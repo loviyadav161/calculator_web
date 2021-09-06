092c89bf446e869910b60ea32d8544f4a20fc9f6
loviyadav161 <alovish001@gmail.com>
2021-09-06 07:52:30 +0530

    added js folder in html

--- js/binary.js
+++
@@ -1,6 +0,0 @@
-let a = document.getElementById('input');
-document.getElementById('go').addEventListener('click',bin2dec);
-function bin2dec(){
-    let ans =  parseInt(a.value, 2).toString(10);
-    document.getElementById('output').value = ans;
-}
