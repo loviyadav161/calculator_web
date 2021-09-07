a29d3fae5ba841e30a5aae65dddf329a5a6260b6
loviyadav161 <alovish001@gmail.com>
2021-09-07 08:01:40 +0530

    fixed bug releated to alert

--- html/js/mathOper.js
+++ html/js/mathOper.js
@@ -1,9 +1,10 @@
+console.log('hello')
 let a = document.getElementById('input11');
 let b = document.getElementById('input12');
 document.getElementById('go1').addEventListener('click',addition)
 function addition(){
-    let x =parseInt(a.value,2) ;
-    let y =parseInt(b.value,2)
+    let x =parseInt(a.value, 2);
+    let y =parseInt(b.value, 2);
     if(isNaN(x))
     {
         alert("Please enter 0's and 1's");
@@ -14,16 +15,15 @@ function addition(){
     }
     else
     {
-        console.log("hello");
         document.getElementById('output1').value = x + y;
     }
 }
-a = document.getElementById('input21');
-b = document.getElementById('input22');
+let c = document.getElementById('input21');
+let d = document.getElementById('input22');
 document.getElementById('go2').addEventListener('click',substraction)
 function substraction(){
-    let x =parseInt(a.value,2) ;
-    let y =parseInt(b.value,2)
+    let x =parseInt(c.value,2) ;
+    let y =parseInt(d.value,2)
     if(isNaN(x))
     {
         alert("Please enter 0's and 1's");
@@ -38,12 +38,12 @@ function substraction(){
         document.getElementById('output2').value = x - y;
     }
 }
-a = document.getElementById('input31');
-b = document.getElementById('input32');
+let e = document.getElementById('input31');
+let f = document.getElementById('input32');
 document.getElementById('go3').addEventListener('click',multiply)
 function multiply(){
-    let x =parseInt(a.value,2) ;
-    let y =parseInt(b.value,2)
+    let x =parseInt(e.value,2) ;
+    let y =parseInt(f.value,2)
     if(isNaN(x))
     {
         alert("Please enter 0's and 1's");
@@ -58,12 +58,12 @@ function multiply(){
         document.getElementById('output3').value = x * y;
     }
 }
-a = document.getElementById('input41');
-b = document.getElementById('input42');
+let g = document.getElementById('input41');
+let h = document.getElementById('input42');
 document.getElementById('go4').addEventListener('click',divide)
 function divide(){
-    let x =parseInt(a.value,2) ;
-    let y =parseInt(b.value,2)
+    let x =parseInt(g.value,2) ;
+    let y =parseInt(h.value,2)
     if(isNaN(x))
     {
         alert("Please enter 0's and 1's");
@@ -74,7 +74,6 @@ function divide(){
     }
     else
     {
-        console.log("hello");
         document.getElementById('output4').value = x / y;
     }
 }
